94e0836c8ceba2fe48105aca5cca5c7d37bf31f8
Lex Zavala <lex.zavala7@gmail.com>
2021-07-14 12:39:14 -0500

    interview solution

---
+++ JS/INTERVIEW.js
@@ -0,0 +1,27 @@
+
+// public void swapStrings(String [ ] a, String [ ] b)
+// The output should make it so array a contains the strings of b, and vice vers
+
+let a = ["a", "b", "c"];
+let b = ["d", "e", "f"];
+
+function swapStrings( a, b){
+
+    for (var i = 0; i < a.length; i++){
+        b.push(a[i]);
+        console.log(b);
+    }
+    b.splice(0,2);
+    console.log(b);
+}
+
+swapStrings(a,b);
+
+
+function swapStrings(a, b) {
+    for(var i = 0; i < a.length; i++) {
+        const tempString = a[i];
+        a[i] = b[i];
+        b[i] = tempString;
+    }
+}
